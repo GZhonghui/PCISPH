d1f2414b49f295b1b82d1507a65cdc5383bba986
GZhonghui <mail@gzher.com>
2024-12-18 19:40:06 +0900

    misc

--- Fluid/SPH/NeighborhoodSearcher.py
+++ Fluid/SPH/NeighborhoodSearcher.py
@@ -1,8 +1,15 @@
 import taichi as ti
 
 class NeighborhoodSearcher:
-    def __init__(self):
-        ...
+    def __init__(self, domain_start: list, domain_end: list):
+        self.domain_start = domain_start
+        self.domain_end = domain_end
 
     def __del__(self):
+        ...
+
+    def clear(self):
+        ...
+
+    def reg_point(self):
         ...
--- Fluid/_basic/math.py
+++ Fluid/_basic/math.py
@@ -1,3 +1,6 @@
+import taichi as ti
+import copy, math
+
 eps = 1e-6
 
 world_up = (0.0, 1.0, 0.0)
@@ -6,3 +9,13 @@ def calc_radius(particle_mass: float, density: float) -> float:
     particle_cnt = int(density // particle_mass)
     particle_per_axis = int(particle_cnt ** (1/3) + 0.5)
     return 0.5 / particle_per_axis
+
+def in_range(point: list, start: list, end: list) -> list:
+    dim = len(point)
+    in_point = [0] * dim
+    is_in = True
+    for i in range(dim):
+        if point[i] < start[i] - eps or point[i] > end[i] + eps:
+            is_in = False
+        in_point[i] = math.min(math.max(point[i], start[i]), end[i])
+    return [is_in, in_point]
